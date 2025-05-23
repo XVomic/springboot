e250ab5d6eafa04e58328d96c5478c43390af4a8
xxchheh <2961166903@qq.com>
2025-05-23 20:41:11 +0800

    初步集成日志框架logback 解决接口调用bug

--- src/main/java/com/xxc/learnboot/controller/UserController.java
+++ src/main/java/com/xxc/learnboot/controller/UserController.java
@@ -5,19 +5,25 @@ import com.xxc.learnboot.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.logging.Logger;
 
 @Controller
 @ResponseBody
 @RequestMapping("/user")
 public class UserController {
 
+    public static final Logger logger = Logger.getLogger(String.valueOf(UserController.class));
+
     @Autowired
     private UserService userService;
 
     @PostMapping("/insert")
-    public User insert(User user) {
+    public User insert(@RequestBody User user) {
+        logger.info(user.toString());
         return this.userService.insert(user);
     }
 
